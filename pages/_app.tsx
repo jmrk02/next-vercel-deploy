7970b9111e3484b78b6a4c386161f35f6569a69b
jmrk02 <jeanmarcoe2@gmail.com>
2023-08-21 20:46:07 -0500

    pre-deployment vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from "next";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: JSX.Element) => JSX.Element;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+  // return <Component {...pageProps} />
+  return getLayout(<Component {...pageProps} />);
 }
